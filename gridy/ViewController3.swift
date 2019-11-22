7df138338e3a143306482b56a387d839487ba504
Oliver <olivermcconnie@outlook.com>
2019-11-22 16:09:59 +0000

    Update 22nd
    update 22nd

--- gridy/ViewController3.swift
+++ gridy/ViewController3.swift
@@ -15,89 +15,47 @@ class GridyCell: UICollectionViewCell {
 }
 
 class ViewController3: UIViewController, UICollectionViewDataSource,
-    UICollectionViewDelegate,
-    UIDropInteractionDelegate,
-    UICollectionViewDragDelegate {
+    UICollectionViewDelegate, UIDropInteractionDelegate, UICollectionViewDragDelegate {
     
-   
+    //variables
+    var toReceive = [UIImage]() // keep correct order for comparison
+    var collectionOne = [UIImage]() // image split into pieces, will reorder
+    var collectionTwo = [UIImage]() // holds placeholders in drop spaces
     var collectionViewIndexPath: IndexPath?
-    var moves = 0
-
-    var toReceive = [UIImage]()
-    var collectionOne = [UIImage]()
-    var collectionTwo = [UIImage]()
-
-    // arrays of tiles from split image
-    var originalTiles = [UIImage]() // image split into pieces, will reorder
-    var originalTilesBeforeShuffle = [UIImage]() // keep correct order for comparison
-    var targetTiles = [UIImage]() // holds placeholders in drop spaces
-    
-    //    @IBOutlet weak var hintImage: UIImageView!
-    //    @IBOutlet weak var hintView: UIView!
+    var rightMoves: Int = 0
+ 
+    //outlets
     @IBOutlet weak var movesCount: UILabel!
     @IBOutlet weak var firstCollectionView: UICollectionView!
     @IBOutlet weak var gameCollectionView: UICollectionView!
     
-
-    
-    
-    //    @IBAction func showHint(_ sender: Any) {
-    //        // shows hint for 2 seconds and updates move count
-    //        hintImage.image = editedImage
-    //        hintView.isHidden = false
-    //        perform(#selector(hideHintView), with: nil, afterDelay: 2)
-    //        moves += 1
-    //        movesCount.text = String(moves)
-    //    }
-    
-    
-    public var editedImage: UIImage? // for screenshot from last view (screenshot in A)
-    let gridSize = 4
-
-
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-    
+//
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionOne = toReceive
         collectionOne.shuffle()
         print(":: DEBUGGING CODE :: \(toReceive.count)")
-//    }
-    
-    // setting collection view delegates & data sources
-    firstCollectionView.dataSource = self
-//    gameCollectionView.dataSource = self
-    firstCollectionView.dragDelegate = self
-//    firstCollectionView.dropDelegate = self
-    gameCollectionView.dragDelegate = self
-//    gameCollectionView.dropDelegate = self
 
+    // setting collection view delegates & data sources
+   firstCollectionView.dataSource = self
+   firstCollectionView.dragDelegate = self
+   gameCollectionView.dragDelegate = self
+//   gameCollectionView.dropDelegate = ?
+//   firstCollectionView.dropDelegate = ?
+//   gameCollectionView.dataSource = ?
+        
     // not allowing collection views to scroll
     firstCollectionView.isScrollEnabled = false
     gameCollectionView.isScrollEnabled = false
-
-//    // fill tile arrays
-//    if let editedImage = editedImage {
-////        let tiles = editedImage.slice (into: 4)
-//        originalTilesBeforeShuffle = tiles
-//        originalTiles.removeAll()
-//        originalTiles = tiles
-//        originalTiles.shuffle()
-//        firstCollectionView.reloadData()
-
-//        for _ in 1...16 {
-//            targetTiles.append(UIImage(named: "placeholder")!)
-//        }
-
+   
         gameCollectionView.reloadData()
-//    }
 
 //     enabling drag on collection views
     firstCollectionView.dragInteractionEnabled = true
     gameCollectionView.dragInteractionEnabled = true
-
 }
 
      func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -112,192 +70,100 @@ class ViewController3: UIViewController, UICollectionViewDataSource,
 
  
 func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
-    let item = collectionView == firstCollectionView ? originalTiles[indexPath.row] : targetTiles[indexPath.row]
+    let item = collectionView == firstCollectionView ?
+    toReceive[indexPath.row] : collectionTwo[indexPath.row]
     let itemProvider = NSItemProvider(object: item as UIImage)
     let dragItem = UIDragItem(itemProvider: itemProvider)
     dragItem.localObject = item
     return [dragItem]
         
     }
-      // moving tiles between collection views adn updating contents
-         private func moveItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView) {
-             let items = coordinator.items
-             
+    
+    // Drop Session
+        func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal {
+            if destinationIndexPath?.row == 16 || destinationIndexPath?.row == 17 {
+                return UICollectionViewDropProposal(operation: .forbidden)
+            } else if collectionView === firstCollectionView  {
+                return UICollectionViewDropProposal(operation: .move, intent: .insertIntoDestinationIndexPath)
+            } else if collectionView === gameCollectionView && gameCollectionView.hasActiveDrag {
+                return UICollectionViewDropProposal(operation: .move, intent: .insertIntoDestinationIndexPath)
+            } else {
+                return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+            }
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+            let dip: IndexPath
+            if let indexPath = coordinator.destinationIndexPath {
+                dip = indexPath
+            } else {
+                let section = collectionView.numberOfSections - 1
+                let row = collectionView.numberOfItems(inSection: section)
+                dip = IndexPath(row: row, section: section)
+            }
+            if dip.row == 16 || dip.row == 17 {
+                return
+            }
+            if collectionView === gameCollectionView {
+                moveItems(coordinator: coordinator, destinationIndexPath: dip, collectionView: collectionView)
+            } else if collectionView === firstCollectionView {
+                reorderItems(coordinator: coordinator, destinationIndexPath: dip, collectionView: collectionView)
+            } else {
+                return
+            }
+        }
+        
+        // drag and drop functions
+        private func moveItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView) {
+            let items = coordinator.items
             collectionView.performBatchUpdates({
-                 let dragItem = items.first!.dragItem.localObject as! UIImage
-                 if dragItem === originalTilesBeforeShuffle[destinationIndexPath.item] {
-                     self.targetTiles.insert(items.first!.dragItem.localObject as! UIImage, at: destinationIndexPath.row)
-                     gameCollectionView.insertItems(at: [destinationIndexPath])
-                     
-                     if let selected = collectionViewIndexPath {
-                         originalTiles.remove(at: selected.row)
-                         if let temp = UIImage(named: "placeholder") {
-                             let blank = temp
-                             originalTiles.insert(blank, at: selected.row)
-                         }
-                         firstCollectionView.reloadData()
-                         
-                     }
-                     
-                 }
-             })
-             
-             collectionView.performBatchUpdates({
-                 if items.first!.dragItem.localObject as! UIImage === originalTilesBeforeShuffle[destinationIndexPath.item] {
-                     targetTiles.remove(at: destinationIndexPath.row + 1)
-                     let nextIndexPath = NSIndexPath(row: destinationIndexPath.row + 1, section: 0)
-                     gameCollectionView.deleteItems(at: [nextIndexPath] as [IndexPath])
-                     gameCollectionView.reloadData()
-                 }
-             })
-             
-           
-          
-       
-       
+                let dragItem = items.first!.dragItem.localObject as! UIImage
+                if dragItem === toReceive[destinationIndexPath.item] {
+                self.collectionTwo.insert(items.first!.dragItem.localObject as! UIImage, at: destinationIndexPath.row)
+                    gameCollectionView.insertItems(at: [destinationIndexPath])
+                    if let selected = collectionViewIndexPath {
+                        collectionOne.remove(at: selected.row)
+                        if let temp = UIImage(named: "Blank") {
+                            let blank = temp
+                            collectionOne.insert(blank, at: selected.row)
+                        }
+                        firstCollectionView.reloadData()
+                    }
+                
+                    }
+                })
+            
+            collectionView.performBatchUpdates({
+                if items.first!.dragItem.localObject as! UIImage === toReceive[destinationIndexPath.item] {
+                    self.collectionTwo.remove(at: destinationIndexPath.row + 1)
+                    let nextIndexPath = NSIndexPath(row: destinationIndexPath.row + 1, section: 0)
+                    gameCollectionView.deleteItems(at: [nextIndexPath] as [IndexPath])
+                }
+            })
+            coordinator.drop(items.first!.dragItem, toItemAt: destinationIndexPath)
+            if rightMoves == collectionTwo.count {
+                print ("gameover screen")
+            }
+        }
+        
+        func reorderItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView) {
+            let items = coordinator.items
+            if items.count == 1, let item = items.first, let sourceIndexPath = item.sourceIndexPath {
+                var dIndexPath = destinationIndexPath
+                if dIndexPath.row >= collectionView.numberOfItems(inSection: 0)
+                {
+                    dIndexPath.row = collectionView.numberOfItems(inSection: 0) - 1
+                }
+                collectionView.performBatchUpdates({
+                    self.collectionOne.remove(at: sourceIndexPath.row)
+                    self.collectionOne.insert(item.dragItem.localObject as! UIImage, at: dIndexPath.row)
+                    collectionView.deleteItems(at: [sourceIndexPath])
+                    collectionView.insertItems(at: [dIndexPath])
+                })
+                coordinator.drop(item.dragItem, toItemAt: dIndexPath)
+            }
+        
+        }
     }
     
-    }
-
- 
-
-
-
-
-
-//    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
-//        <#code#>
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
-//        <#code#>
-//}
-//
-//// moving tiles between collection views adn updating contents
-//  private func moveItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView) {
-//      let items = coordinator.items
-//
-//      collectionView.performBatchUpdates({
-//          let dragItem = items.first!.dragItem.localObject as! UIImage
-//          if dragItem === originalTilesBeforeShuffle[destinationIndexPath.item] {
-//              self.targetTiles.insert(items.first!.dragItem.localObject as! UIImage, at: destinationIndexPath.row)
-//              targetCollectionView.insertItems(at: [destinationIndexPath])
-//
-//              if let selected = collectionViewIndexPath {
-//                  originalTiles.remove(at: selected.row)
-//                  if let temp = UIImage(named: "placeholder") {
-//                      let blank = temp
-//                      originalTiles.insert(blank, at: selected.row)
-//                  }
-//                  tileCollectionView.reloadData()
-//
-//              }
-//
-//          }
-//      })
-//
-//      collectionView.performBatchUpdates({
-//          if items.first!.dragItem.localObject as! UIImage === originalTilesBeforeShuffle[destinationIndexPath.item] {
-//              targetTiles.remove(at: destinationIndexPath.row + 1)
-//              let nextIndexPath = NSIndexPath(row: destinationIndexPath.row + 1, section: 0)
-//              targetCollectionView.deleteItems(at: [nextIndexPath] as [IndexPath])
-//              targetCollectionView.reloadData()
-//          }
-//      })
-//
-//      coordinator.drop(items.first!.dragItem, toItemAt: destinationIndexPath)
-//
-//      // adding to move count and updating label
-//      moves += 1
-//      movesCount.text = String(moves)
-//
-//      // testing for win after each move
-//      if originalTiles.allSatisfy({$0 == UIImage(named: "placeholder")}) {
-//          print("You win!")
-//          performSegue(withIdentifier: "PuzzletoWinSegue", sender: nil)
-//      }
-//
-//  }
-//
-//  @objc func hideHintView() {
-//      hintView.isHidden = true
-//  }
-//
-//
-//
-//
-    
-
-    
-
-
-    
-//    -------------
-    
-    //MARK: - IBOutlets
-//      @IBOutlet weak var tileView: UICollectionView!
-//      @IBOutlet weak var puzzleBoard: UICollectionView!
-//      @IBOutlet weak var scoreLabel: UILabel!
-//      @IBOutlet weak var popUpView: UIImageView!
-//      @IBOutlet weak var newGameButton: UIButton!
-//      @IBAction func switchButtonOn(_ sender: UISwitch) {
-//      }
-//
-//      //MARK: - Variables
-//      var toReceive = [UIImage]()
-//      var tileViewImages :[UIImage]!
-//      var puzzleBoardImages = [UIImage]()
-//      var fixedImages = [UIImage(named: "Blank"), UIImage(named: "Gridy-lookup")]
-//      var moves: Int = 0
-//      var rightMoves: Int = 0
-//      var popUpImage = UIImage()
-//      var isSelected: IndexPath?
-//      var audioPlayer: AVAudioPlayer?
-//
-//      //MARK: - View Functions
-//      override func viewDidLoad() {
-//          super.viewDidLoad()
-//
-//          tileViewImages = toReceive
-//          tileViewImages.shuffle()
-//          tileView.reloadData()
-//          popUpView.image = popUpImage
-//          popUpView.isHidden = true
-//          scoring(moves: moves)
-//          for image in fixedImages {
-//              if let image = image {
-//                  tileViewImages.append(image)
-//              }
-//          }
-//          tileView.dragInteractionEnabled = true
-//          puzzleBoard.dragInteractionEnabled = true
-//          if puzzleBoardImages.count == 0 {
-//              if let blank = UIImage(named: "Blank") {
-//                  var temp = [UIImage]()
-//                  for _ in toReceive {
-//                      temp.append(blank)
-//                  }
-//                  puzzleBoardImages = temp
-//                  puzzleBoard.reloadData()
-//              }
-//          }
-//      }
-//
-//      override func didReceiveMemoryWarning() {
-//          super.didReceiveMemoryWarning()
-//      }
-//
-//
-//-----------------
-    
- 
-  
-
-
-
-
-
-   
-    
- 
 
