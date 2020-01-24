75e09c9550b5f8c1992f9384523a7de88b30a336
Oliver <olivermcconnie@outlook.com>
2020-01-24 17:19:35 +0000

    update
    update

--- gridy/Right/BottomCollectionView.swift
+++ gridy/Right/BottomCollectionView.swift
@@ -12,8 +12,13 @@ class BottomCollectionView: UICollectionView, UICollectionViewDelegate, UICollec
     
     @IBOutlet weak var largeCellEmptyImage: UIImageView!
     public var correctImages: [UIImage]!
-    public var images: [UIImage] = []
+    public var images: [UIImage]! = [UIImage] ()
     static private let kID = "largeCell"   // Change to your identifier on the storyboard
+    
+    
+         
+    
+    required init?(coder aDecoder: NSCoder) { super.init(coder: aDecoder) }
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
@@ -30,30 +35,57 @@ class BottomCollectionView: UICollectionView, UICollectionViewDelegate, UICollec
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BottomCollectionView.kID, for: indexPath) as! GridyCell
           cell.imageView.image = images[indexPath.item]
         return cell
-        // Configure Cell Here
-        
-      
     }
 }
 
 extension BottomCollectionView: UICollectionViewDropDelegate {
     
+    
     func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
-            let destinationIndexPath: IndexPath
-            if let indexPath = coordinator.destinationIndexPath {
-                destinationIndexPath = indexPath
-            } else {
-                destinationIndexPath = IndexPath(row: 0, section: 0)
-            }
-            collectionView.moveItem(at: coordinator.items.first!.sourceIndexPath!, to: destinationIndexPath)
-   }
-}
+ print ("correctImages [index.row]")
+        guard let index = coordinator.destinationIndexPath else { return }
+        guard let dropItem = coordinator.items.first else { return
             
-        
-        // Configure Cell Here
-        
-        
-        // Configure drop here
-        // Handle score here
+    
+    func dragItems(for indexPath: IndexPath) -> [UIDragItem] {
+        let item = images[indexPath.item]
+        let itemProvider = NSItemProvider(object: item as UIImage)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+            dragItem.localObject = item
+        return [dragItem]
+        }
         
  
+    func addItem(_ newItem: UIImage, at index: Int) {
+        images.insert(newItem, at: index)
+        }
+        
+    func deleteItem(at sourceIndex: Int) {
+        images.remove(at: sourceIndex)
+        }
+        
+    func moveItem(at sourceIndex: Int, to destinationIndex: Int) {
+        guard sourceIndex != destinationIndex else { return }
+        let item = images[sourceIndex]
+           images.insert(item, at: destinationIndex)
+            images.remove(at: sourceIndex)
+        }
+        
+    func swapItem(at sourceIndex: Int, to destinationIndex: Int) {
+            guard sourceIndex != destinationIndex else { return }
+        images.swapAt(sourceIndex, destinationIndex)
+        }
+        
+    func getItemAtIndex(indexPath: Int) -> UIImage? {
+        return images[indexPath]
+        }
+      }
+  }
+}
+            
+        /// Check if image at drop == correctimages[index.row]
+        /// If it is, you are going to drop it in at index.row
+        
+
+    
+    
